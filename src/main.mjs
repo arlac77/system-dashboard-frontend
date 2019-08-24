77f63cbc57e37ca065c17f51bee91f82fab4a75c
Markus Felten <Markus.Felten@gmx.de>
2019-08-24 22:01:36 +0200

    fix: gardening

--- src/main.mjs
+++ src/main.mjs
@@ -1,8 +1,5 @@
-import App from './App.svelte';
+import App from "./App.svelte";
 
-const app = new App({
-  target: document.body,
-  props: {},
+export default new App({
+  target: document.body
 });
-
-export default app;
