1309e6846cbed4f5615729673df0418e322620b1
Markus Felten <Markus.Felten@gmx.de>
2019-09-05 12:44:32 +0200

    fix: rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,33 @@
-import { config } from "./package.json";
-
-import { spawn } from "child_process";
 import svelte from "rollup-plugin-svelte";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import json from "rollup-plugin-json";
 import { terser } from "rollup-plugin-terser";
 import copy from "rollup-plugin-copy";
+import { spawn } from "child_process";
+import { config } from "./package.json";
 
 const production = !process.env.ROLLUP_WATCH;
 const dist = "public";
+
+if (!production) {
+  const ls = spawn("./node_modules/.bin/light-server", [
+    "-s",
+    dist,
+    "--servePrefix",
+    config.urlPrefix,
+    "--historyindex",
+    config.urlPrefix + "/index.html",
+    "-p",
+    "5000",
+    "--proxypath",
+    config.api,
+    "-x",
+    config.proxyTarget,
+    "-w", `${dist}/*.mjs,${dist}/*.css`
+  ]);
+  ls.stdout.pipe(process.stdout);
+}
 
 export default {
   input: "src/main.mjs",
@@ -25,7 +43,7 @@ export default {
     svelte({
       dev: !production
     }),
-    resolve(),
+    resolve({ browser: true }),
     commonjs(),
     json({
       preferConst: true,
