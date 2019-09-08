5e11a00cfaeb6fa68d6ad99b70f81720bd06d119
Markus Felten <Markus.Felten@gmx.de>
2019-09-09 00:17:24 +0200

    fix: add router

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,39 @@
 <script>
-  import Journal  from './components/Journal.svelte';
+  import { Outlet, Link } from "svelte-guard-history-router";
+  import Journal from "./components/Journal.svelte";
+  import { router, session } from "./main.mjs";
+
+  function logout() {
+    session.invalidate();
+  }
 </script>
 
-<style>
-</style>
+<nav>
+  <Link href="/">
+    <img class="logo" src="hook_ci.svg" alt="Hook CI" />
+    <h2>Dashboard</h2>
+  </Link>
+  <ul>
+    <li>
+      <Link href="/about">
+        <h3>About</h3>
+      </Link>
+    </li>
+    <li>
+      {#if $session.isValid}
+        <a href="/" on:click|preventDefault={logout}>
+          Logout {$session.username}
+        </a>
+      {:else}
+        <Link href="/login">
+          <h3>Login</h3>
+        </Link>
+      {/if}
+    </li>
+  </ul>
+</nav>
+<main>
+  <Outlet {router}>nothing there</Outlet>
 
-<Journal />
+  <Journal />
+</main>
--- src/main.mjs
+++ src/main.mjs
@@ -1,4 +1,17 @@
+import { Router, route, NotFound, Guard } from "svelte-guard-history-router";
+import { Session } from "svelte-session-manager";
+
+
 import App from "./App.svelte";
+
+export const session = new Session(localStorage);
+
+export const router = new Router(
+  [
+    route("*", NotFound)
+  ],
+  config.urlPrefix
+);
 
 export default new App({
   target: document.body
