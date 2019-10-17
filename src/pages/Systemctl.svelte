81a9188fec25e034873141bf689c3b9ac047d5c0
Markus Felten <Markus.Felten@gmx.de>
2019-10-17 11:52:07 +0200

    fix: add auth header

--- src/pages/Systemctl.svelte
+++ src/pages/Systemctl.svelte
@@ -1,15 +1,19 @@
 <script>
   import { onMount } from "svelte";
   import { config } from "../../package.json";
+  import { session } from "../main.mjs";
 
   let status = "";
 
   onMount(async () => {
-    const res = await fetch(config.api + "/systemctl/status");
+    const res = await fetch(config.api + "/systemctl/status", {
+      headers: {
+        ...session.authorizationHeader
+      }
+    });
     status = await res.text();
   });
 </script>
 
 <h3>Systemctl</h3>
-
 {status}
