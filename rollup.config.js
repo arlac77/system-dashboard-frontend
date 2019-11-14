ea0aaacb4e068f59ea4e061e7c97f3525ac26fc1
Markus Felten <Markus.Felten@gmx.de>
2019-11-14 11:50:01 +0100

    chore(rollup): sync from template

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,15 @@
-import dev from "rollup-plugin-dev";
 import svelte from "rollup-plugin-svelte";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
-import json from "rollup-plugin-json";
+import json from "@rollup/plugin-json";
 import { terser } from "rollup-plugin-terser";
+import dev from "rollup-plugin-dev";
 import copy from "rollup-plugin-copy";
 import { config } from "./package.json";
 
 const production = !process.env.ROLLUP_WATCH;
 const dist = "public";
 const port = 5000;
-
 
 export default {
   input: "src/main.mjs",
@@ -19,23 +18,34 @@ export default {
     format: "esm",
     file: `${dist}/bundle.mjs`
   },
-  plugins: [copy({
-    targets: [{ src: "node_modules/mf-styling/global.css", dest: dist }]
-  }), svelte({
-    dev: !production,
-    css: css => {
-              css.write(`${dist}/bundle.css`);
-          }
-  }), resolve({ browser: true }), commonjs(), json({
-    preferConst: true,
-    compact: true
-  }), production && terser(), dev({
-    port,
-    dirs: [dist],
-    spa: `${dist}/index.html`,
-    basePath: config.base,
-    proxy: { [`${config.api}/*`]: [config.proxyTarget, { https: true }] }
-  })],
+  plugins: [
+    copy({
+      targets: [
+        { src: 'node_modules/mf-styling/global.css', dest: dist }
+      ]
+    }),
+    svelte({
+      dev: !production,
+      css: css => {
+				css.write(`${dist}/bundle.css`);
+			}
+    }),
+
+    resolve({ browser: true }),
+    commonjs(),
+    json({
+      preferConst: true,
+      compact: true
+    }),
+    production && terser(),
+    dev({
+      port,
+      dirs: [dist],
+      spa: `${dist}/index.html`,
+      basePath: config.base,
+      proxy: { [`${config.api}/*`]: [config.proxyTarget, { https: true }] }
+    })
+  ],
   watch: {
     clearScreen: false
   }
