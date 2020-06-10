9758ddaea1cd45831b6ae385076277186d302a42
Markus Felten <Markus.Felten@gmx.de>
2020-06-10 17:06:14 +0200

    feat: add logger range

--- src/pages/Journal.svelte
+++ src/pages/Journal.svelte
@@ -3,10 +3,29 @@
   import { session } from "../main.mjs";
   import journalUrlPrefix from "consts:journalUrlPrefix";
 
-  async function* logEntries() {
-    const response = await fetch(journalUrlPrefix + "/entries", {
+/*
+curl -H 'Range: entries=:1000' -H 'Accept: application/json' http://localhost:5000/services/journal/entries
+*/
+  async function* logEntries(range) {
+    if (!range) {
+      /*  if (localStorage.cursor != null && localStorage.cursor != "")
+        range = localStorage.cursor + ":0";
+      else range = "";
+      */
+      range = "";
+    }
+
+    const qp = {
+    //  _SYSTEMD_UNIT: 'sshd.service'
+     };
+
+    const query = Object.entries(qp).map(([k,v]) => `${k}=${escape(v)}`).join('&')
+
+    const response = await fetch(journalUrlPrefix + "/entries?" + query, {
       headers: {
-        ...session.authorizationHeader
+        ...session.authorizationHeader,
+        //        Accept: "application/json",
+        Range: "entries=" + range + ":1000"
       }
     });
     yield* lineIterator(response.body.getReader());
