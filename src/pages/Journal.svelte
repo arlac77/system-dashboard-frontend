7fa7580fb34dacb032edcb5f67acef827872732c
Markus Felten <Markus.Felten@gmx.de>
2020-06-11 02:20:08 +0200

    feat: json logs

--- src/pages/Journal.svelte
+++ src/pages/Journal.svelte
@@ -3,29 +3,45 @@
   import { session } from "../main.mjs";
   import journalUrlPrefix from "consts:journalUrlPrefix";
 
-/*
-curl -H 'Range: entries=:1000' -H 'Accept: application/json' http://localhost:5000/services/journal/entries
+  // https://www.freedesktop.org/software/systemd/man/systemd.journal-fields.html#
+  const fields = [
+    "MESSAGE",
+    "MESSAGE_ID",
+    "PRIORITY",
+    "_HOSTNAME",
+    "_PID",
+    "_UID",
+    "_GID",
+    "__REALTIME_TIMESTAMP"
+  ];
+
+  /*
+curl -H 'Range: entries=:1000' -H 'Accept: application/json' http://localhost:5000/services/journal/entries?follow
 */
-  async function* logEntries(range) {
-    if (!range) {
-      /*  if (localStorage.cursor != null && localStorage.cursor != "")
+  async function* logEntries() {
+    let numberOfEntries = 1000;
+    let skipEntries = 0;
+    let cursor = "s=9d01acbf99b94aa7847c629f5f36bbaf;i=c74e2d;b=c39b51befc314bb6b108e94253943e81;m=270556d2a1;t=5a790df4a965b;x=e86637883bed921f";
+
+    /*  if (localStorage.cursor != null && localStorage.cursor != "")
         range = localStorage.cursor + ":0";
-      else range = "";
       */
-      range = "";
-    }
 
     const qp = {
-    //  _SYSTEMD_UNIT: 'sshd.service'
-     };
+      //boot:undefined,
+      follow: undefined
+      //  _SYSTEMD_UNIT: 'sshd.service'
+    };
 
-    const query = Object.entries(qp).map(([k,v]) => `${k}=${escape(v)}`).join('&')
+    const query = Object.entries(qp)
+      .map(([k, v]) => `${k}${v === undefined ? "" : "=" + escape(v)}`)
+      .join("&");
 
     const response = await fetch(journalUrlPrefix + "/entries?" + query, {
       headers: {
         ...session.authorizationHeader,
-        //        Accept: "application/json",
-        Range: "entries=" + range + ":1000"
+              Accept: "application/json",
+        Range: `entries=${cursor}`// :${skipEntries}:${numberOfEntries}`
       }
     });
     yield* lineIterator(response.body.getReader());
