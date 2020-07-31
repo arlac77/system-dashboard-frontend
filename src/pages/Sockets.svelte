5acb498eefe503c9601463ff180c8a3d3ad24819
Markus Felten <Markus.Felten@gmx.de>
2020-07-31 19:34:05 +0200

    fix: more unit attributes

--- src/pages/Sockets.svelte
+++ src/pages/Sockets.svelte
@@ -1,29 +1,30 @@
 <script>
-    import { Link } from "svelte-guard-history-router";
-  
-    export let router;
-  
-    const route = router.route;
-  </script>
-  
-  <table class="bordered striped hoverable">
-    <thead>
+  import { Link } from "svelte-guard-history-router";
+
+  export let router;
+
+  const route = router.route;
+</script>
+
+<table class="bordered striped hoverable">
+  <thead>
+    <tr>
+      <th aria-sort="none">Listen</th>
+      <th aria-sort="none">Units</th>
+      <th aria-sort="none">Activates</th>
+    </tr>
+  </thead>
+  <tbody>
+    {#each $route as socket}
       <tr>
-        <th aria-sort="none">Listen</th>
-        <th aria-sort="none">Unit</th>
-        <th aria-sort="none">Activates</th>
+        <td>{socket.listen}</td>
+        <td>
+          {#each socket.units as unit}
+            <Link href="/unit/{unit}">{unit}</Link>
+          {/each}
+        </td>
+        <td>{socket.activates}</td>
       </tr>
-    </thead>
-    <tbody>
-      {#each $route as socket}
-        <tr>
-          <td>{socket.listen}</td>
-          <td>
-            <Link href="/unit/{socket.unit}">{socket.unit}</Link>
-          </td>
-          <td>{socket.activates}</td>
-        </tr>
-      {/each}
-    </tbody>
-  </table>
-  
+    {/each}
+  </tbody>
+</table>
--- src/pages/Unit.svelte
+++ src/pages/Unit.svelte
@@ -6,9 +6,11 @@
   
   {#if $route}
   <h3>Unit {$route.unit}</h3>
+  {$route.description}
   {$route.load}
   {$route.active}
   {$route.sub}
-  {$route.description}
+  {$route.mainPid}
+  {$route.triggeredBy}
 {:else}No such unit{/if}
   
