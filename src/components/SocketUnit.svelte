a2a560d651b12ee3c88092ef784ee587aec59aff
Markus Felten <Markus.Felten@gmx.de>
2020-08-17 13:52:26 +0200

    fix: add cgroup to SocketUnit

--- src/components/SocketUnit.svelte
+++ src/components/SocketUnit.svelte
@@ -1,9 +1,13 @@
 <script>
   import { ObjectLink } from "svelte-guard-history-router";
   import TriggerUnit from "./TriggerUnit.svelte";
+  import CGroup from "./CGroup.svelte";
   export let unit;
 </script>
 
 <div>Listen: {unit.listen}</div>
 
 <TriggerUnit {unit} />
+{#if unit.CGroup}
+  <CGroup cgroup={unit.CGroup} />
+{/if}
