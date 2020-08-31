a597a3d38d2231510d0b1368ea04e9855ae75253
Markus Felten <Markus.Felten@gmx.de>
2020-08-31 21:07:53 +0200

    fix: peer styling

--- src/components/Peer.svelte
+++ src/components/Peer.svelte
@@ -2,7 +2,18 @@
   export let peer;
 </script>
 
-<div>
+<style>
+
+.peer {
+  display: inline-block;
+  background-color: var(--button-disabled-background-color);
+  border-radius: 0.5em;
+  padding: 0.2em;
+  margin: 0.2em;
+}
+</style>
+
+<div class="peer">
   {peer.host}:{peer.port}
   {#if peer.referrer}(referrer {peer.referrer.host}:{peer.referrer.port}){/if}
   {#if peer.to}(to {peer.to.host}:{peer.to.port}){/if}
