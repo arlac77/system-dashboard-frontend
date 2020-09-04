e1bacebf295cc1d51fe6e87728d9400478233546
Markus Felten <Markus.Felten@gmx.de>
2020-09-04 13:55:57 +0200

    fix: code cleanup

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -12,8 +12,6 @@
   import name from "consts:name";
   import version from "consts:version";
   import description from "consts:description";
-  import base from "consts:base";
-  import api from "consts:api";
   import api_ws from "consts:api_ws";
 
   const protocols = ["access_token", session.access_token];
