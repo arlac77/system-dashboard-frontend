add452410e5b216faba6fa88891b2f953a1945b3
Markus Felten <Markus.Felten@gmx.de>
2020-09-19 22:56:44 +0200

    fix: use initializeServiceWorker from svelte-common

--- src/main.mjs
+++ src/main.mjs
@@ -1,30 +1,8 @@
-import { readable } from "svelte/store";
+import { initializeServiceWorker } from "svelte-common";
 import { Session } from "svelte-session-manager";
 import App from "./App.svelte";
 
 export const session = new Session(localStorage);
-
-export function initializeServiceWorker(script) {
-  const serviceWorker = readable({ state: "unknown" }, set => {
-    navigator.serviceWorker
-      .register(script)
-      .then(serviceWorkerRegistration => {
-        // https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/update
-        for (const state of ["active", "waiting", "installing"]) {
-          const sw = serviceWorkerRegistration[state];
-          if (sw) {
-            set(sw);
-            sw.onstatechange = event => set(sw);
-            return;
-          }
-        }
-      });
-
-    return () => {};
-  });
-
-  return { serviceWorker };
-}
 
 const { serviceWorker } = initializeServiceWorker("bundle.service-worker.mjs");
 export { serviceWorker };
