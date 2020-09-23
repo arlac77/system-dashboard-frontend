eaf3a6929fab491b27ca5bce9ccda726bd97b36f
Markus Felten <Markus.Felten@gmx.de>
2020-09-23 23:59:24 +0200

    fix: do not cache auth requests

--- src/service-worker.mjs
+++ src/service-worker.mjs
@@ -39,6 +39,7 @@ self.addEventListener("fetch", event => {
 
 	// don't try to handle e.g. data: URIs
 	if (!url.protocol.startsWith('http')) return;
+	if (!url.pathname.endsWith('api/authenticate')) return;
 
 	// ignore dev server requests
 	if (url.hostname === self.location.hostname && url.port !== self.location.port) return;
