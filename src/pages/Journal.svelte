0bade8f4f409babc57057e2666ae50e31ec972f5
Markus Felten <Markus.Felten@gmx.de>
2020-10-09 01:22:03 +0200

    fix: use updated log source api

--- src/pages/Journal.svelte
+++ src/pages/Journal.svelte
@@ -1,5 +1,4 @@
 <script>
-  import { onDestroy } from "svelte";
   import { LogView } from "svelte-log-view";
   import { lineIterator } from "reader-line-iterator";
   import JournalEntry from "../components/JournalEntry.svelte";
@@ -22,72 +21,75 @@
 
   export let query = {};
   export let minEntries = 20;
-  
+
   let follow;
   let entries = [];
 
-  const controller = new AbortController();
-  const signal = controller.signal;
+  let controller;
 
-  onDestroy(() => controller.abort());
+  const source = {
+    abort: async () => controller.abort(),
 
-  async function* fetchEntries(cursorEntry, direction) {
-    async function* _fetchEntries(Range, params = {}) {
-      try {
-        const search = Object.entries(params)
-          .map(([k, v]) => `${k}${v === undefined ? "" : "=" + escape(v)}`)
-          .join("&");
+    fetch: async function* (cursorEntry, direction) {
+      if (controller) {
+        controller.abort();
+      }
 
-        const response = await fetch(journalApi + "/entries?" + search, {
-          signal,
-          //   mode: "same-origin",
-          headers: {
-            ...session.authorizationHeader,
-            Accept: "application/json",
-            //   "Accept-Encoding": "gzip,identity",
-            Range
+      controller = new AbortController();
+
+      async function* _fetchEntries(Range, params = {}) {
+        try {
+          const search = Object.entries(params)
+            .map(([k, v]) => `${k}${v === undefined ? "" : "=" + escape(v)}`)
+            .join("&");
+
+          const response = await fetch(journalApi + "/entries?" + search, {
+            signal: controller.signal,
+            headers: {
+              ...session.authorizationHeader,
+              Accept: "application/json",
+              Range
+            }
+          });
+          if (response.ok) {
+            yield* decodeJson(lineIterator(await response.body.getReader()));
           }
-        });
-        if (response.ok) {
-          yield* decodeJson(lineIterator(await response.body.getReader()));
-        }
-      } catch (e) {
-        if (!e instanceof AbortSignal) {
-          throw e;
+        } catch (e) {
+          if (!e instanceof AbortSignal) {
+            throw e;
+          }
         }
       }
+
+      const n = direction == undefined ? minEntries : 1;
+
+      yield* _fetchEntries(
+        `entries=${cursorEntry ? cursorEntry.__CURSOR : ""}:${-n}:${n}`,
+        query
+      );
+
+      if (direction !== undefined) {
+        return;
+      }
+
+      for (let i = 0; i < 60; i++) {
+        try {
+          if (entries.length === 0) {
+            break;
+          }
+
+          const cursor = entries[entries.length - 1].__CURSOR;
+
+          yield* _fetchEntries(`entries=${cursor}`, {
+            ...query,
+            follow: undefined
+          });
+        } catch {}
+      }
     }
-
-    const n = direction == undefined ? minEntries : 1;
-
-    yield* _fetchEntries(
-      `entries=${
-        cursorEntry ? cursorEntry.__CURSOR : ""
-      }:${-n}:${n}`,
-      query
-    );
-
-    if(direction !== undefined) {
-      return;
-    }
-
-    for (let i = 0; i < 60; i++) {
-      try {
-        if (entries.length === 0) {
-          break;
-        }
-
-        const cursor = entries[entries.length - 1].__CURSOR;
-
-        yield* _fetchEntries(`entries=${cursor}`, {
-          ...query,
-          follow: undefined
-        });
-      } catch {}
-    }
-  }
+  };
 </script>
 
-<LogView source={fetchEntries} bind:entries let:entry bind:follow let:selected let:position>
-  <JournalEntry {entry} highlight={selected === position} {follow}/>
+<LogView {source} bind:entries let:entry bind:follow let:selected let:position>
+  <JournalEntry {entry} highlight={selected === position} {follow} />
 </LogView>
