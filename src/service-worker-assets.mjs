53cfa902328f469d0260f7776be330fe5d689f95
Markus Felten <Markus.Felten@gmx.de>
2020-10-09 00:04:02 +0200

    fix: add logo.png to servce worker assets

--- src/service-worker-assets.mjs
+++ src/service-worker-assets.mjs
@@ -2,9 +2,9 @@ import version from "consts:version";
 
 export const ASSET_GENERATION = version;
 export const ASSETS = [
-    "images/logo.svg",
-    "index.html",
-    "bundle.main.mjs",
-    "bundle.main.css"
-  ];
-  
+  "images/logo.svg",
+  "images/logo.png",
+  "index.html",
+  "bundle.main.mjs",
+  "bundle.main.css"
+];
