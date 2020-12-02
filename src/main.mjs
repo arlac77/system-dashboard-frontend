3cd84c05f2792ba12aad48de61e1c2b14d648573
Markus Felten <Markus.Felten@gmx.de>
2020-12-02 21:34:42 +0100

    fix: rollup cyclic imports

--- src/main.mjs
+++ src/main.mjs
@@ -1,8 +1,5 @@
-import { initializeServiceWorker } from "svelte-common";
 import App from "./App.svelte";
-
-const { serviceWorker, serviceWorkerRegistration } = initializeServiceWorker("bundle.service-worker.mjs");
-export { serviceWorker, serviceWorkerRegistration };
+import {} from "./service-worker-registration.mjs";
 
 export default new App({
   target: document.body
