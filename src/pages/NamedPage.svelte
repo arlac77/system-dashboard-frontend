a0388c6365fdc7896f2ef64d9e0f1b9c90291d03
Markus Felten <Markus.Felten@gmx.de>
2025-12-16 13:32:38 +0000

    fix: debug

--- src/pages/NamedPage.svelte
+++ src/pages/NamedPage.svelte
@@ -3,11 +3,13 @@
   
     const route = router.route;
     const object = $route.value;
+
   </script>
   
   {#if object}
     <h3>Named</h3>
-  
+
+    <div>{object}</div>
     <ul>
       {#each Object.entries(object) as e}
         <li>{e[0]} {e[1]}</li>
