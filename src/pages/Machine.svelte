aa640b6750f5eeddac3402a5d11cfa0c51ff6b9d
Markus Felten <Markus.Felten@gmx.de>
2025-12-29 15:59:26 +0100

    fix: use form for machine page

--- src/pages/Machine.svelte
+++ src/pages/Machine.svelte
@@ -1,14 +1,24 @@
-<script>  
-    export let router;
-  
-    const route = router.route;
-    const machine = $route.value;
-  </script>
-  
-  {#if machine}
-    <h3>Machine {machine.name}</h3>
-    State: {machine.state}
-    Jobs: {machine.jobs}
-    Failed: {machine.failed}
-  {:else}No such machine{/if}
-  
+<script>
+  export let router;
+
+  const route = router.route;
+  const machine = $route.value;
+</script>
+
+{#if machine}
+  <h3>Machine {machine.name}</h3>
+  <form>
+    <label>
+      <span>State</span>
+      <input value={machine.state} />
+    </label>
+    <label>
+      <span>Jobs</span>
+      <input value={machine.jobs} />
+    </label>
+    <label>
+      <span>Failed</span>
+      <input value={machine.failed} />
+    </label>
+  </form>
+{:else}No such machine{/if}
